89611bfd80f97459ea9697526c8d5631714728f3
Comf2 <otischaka4@gmail.com>
2023-04-06 09:51:50 -0400

    Finished. Final Time: 1:20

--- name-sorter/Form1.Designer.cs
+++ name-sorter/Form1.Designer.cs
@@ -28,12 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.lstName = new System.Windows.Forms.ListBox();
+            this.lstAlph = new System.Windows.Forms.ListBox();
+            this.lstRvrsAlph = new System.Windows.Forms.ListBox();
+            this.lstLastName = new System.Windows.Forms.ListBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtALph = new System.Windows.Forms.TextBox();
+            this.txtRvrsAlpha = new System.Windows.Forms.TextBox();
+            this.txtLastNames = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSort
+            // 
+            this.btnSort.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnSort.Location = new System.Drawing.Point(62, 12);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(88, 42);
+            this.btnSort.TabIndex = 0;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // lstName
+            // 
+            this.lstName.FormattingEnabled = true;
+            this.lstName.ItemHeight = 15;
+            this.lstName.Location = new System.Drawing.Point(62, 113);
+            this.lstName.Name = "lstName";
+            this.lstName.Size = new System.Drawing.Size(267, 544);
+            this.lstName.TabIndex = 1;
+            // 
+            // lstAlph
+            // 
+            this.lstAlph.FormattingEnabled = true;
+            this.lstAlph.ItemHeight = 15;
+            this.lstAlph.Location = new System.Drawing.Point(382, 113);
+            this.lstAlph.Name = "lstAlph";
+            this.lstAlph.Size = new System.Drawing.Size(267, 544);
+            this.lstAlph.TabIndex = 2;
+            // 
+            // lstRvrsAlph
+            // 
+            this.lstRvrsAlph.FormattingEnabled = true;
+            this.lstRvrsAlph.ItemHeight = 15;
+            this.lstRvrsAlph.Location = new System.Drawing.Point(721, 113);
+            this.lstRvrsAlph.Name = "lstRvrsAlph";
+            this.lstRvrsAlph.Size = new System.Drawing.Size(267, 544);
+            this.lstRvrsAlph.TabIndex = 3;
+            // 
+            // lstLastName
+            // 
+            this.lstLastName.FormattingEnabled = true;
+            this.lstLastName.ItemHeight = 15;
+            this.lstLastName.Location = new System.Drawing.Point(1081, 113);
+            this.lstLastName.Name = "lstLastName";
+            this.lstLastName.Size = new System.Drawing.Size(267, 544);
+            this.lstLastName.TabIndex = 4;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtName.Location = new System.Drawing.Point(62, 75);
+            this.txtName.Name = "txtName";
+            this.txtName.ReadOnly = true;
+            this.txtName.Size = new System.Drawing.Size(100, 29);
+            this.txtName.TabIndex = 5;
+            this.txtName.Text = "Names";
+            // 
+            // txtALph
+            // 
+            this.txtALph.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtALph.Location = new System.Drawing.Point(382, 75);
+            this.txtALph.Name = "txtALph";
+            this.txtALph.ReadOnly = true;
+            this.txtALph.Size = new System.Drawing.Size(107, 29);
+            this.txtALph.TabIndex = 6;
+            this.txtALph.Text = "Alphabetically";
+            // 
+            // txtRvrsAlpha
+            // 
+            this.txtRvrsAlpha.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtRvrsAlpha.Location = new System.Drawing.Point(721, 75);
+            this.txtRvrsAlpha.Name = "txtRvrsAlpha";
+            this.txtRvrsAlpha.ReadOnly = true;
+            this.txtRvrsAlpha.Size = new System.Drawing.Size(163, 29);
+            this.txtRvrsAlpha.TabIndex = 7;
+            this.txtRvrsAlpha.Text = "Reverse Alphabetically";
+            // 
+            // txtLastNames
+            // 
+            this.txtLastNames.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txtLastNames.Location = new System.Drawing.Point(1081, 75);
+            this.txtLastNames.Name = "txtLastNames";
+            this.txtLastNames.ReadOnly = true;
+            this.txtLastNames.Size = new System.Drawing.Size(100, 29);
+            this.txtLastNames.TabIndex = 8;
+            this.txtLastNames.Text = "Last Names";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1409, 812);
+            this.Controls.Add(this.txtLastNames);
+            this.Controls.Add(this.txtRvrsAlpha);
+            this.Controls.Add(this.txtALph);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lstLastName);
+            this.Controls.Add(this.lstRvrsAlph);
+            this.Controls.Add(this.lstAlph);
+            this.Controls.Add(this.lstName);
+            this.Controls.Add(this.btnSort);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Button btnSort;
+        private ListBox lstName;
+        private ListBox lstAlph;
+        private ListBox lstRvrsAlph;
+        private ListBox lstLastName;
+        private TextBox txtName;
+        private TextBox txtALph;
+        private TextBox txtRvrsAlpha;
+        private TextBox txtLastNames;
     }
 }
--- name-sorter/Form1.cs
+++ name-sorter/Form1.cs
@@ -1,10 +1,84 @@
+using System.Drawing.Text;
+using System.Diagnostics;
+using System.Text;
+using System;
+
 namespace name_sorter
 {
     public partial class Form1 : Form
     {
+        public List<string> names = new();
+        public List<string> Alphnames = new();
+        public List<string> rvrsAlphNames = new();
+        public List<string> lastNames = new();
         public Form1()
         {
             InitializeComponent();
         }
+        
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            getNames();
+            Debug.WriteLine(names);
+            //adds all of them to first list
+            for (int i = 0; i < names.Count; i++)
+            {
+                lstName.Items.Add(names[i]);
+                Alphnames.Add(names[i]);
+                
+            }
+            sortAlph();
+            sortRvrsAlph();
+            sortLastName();
+            
+        }
+        //gets all names from file and pushes them to list 
+        private void getNames()
+        {
+            string absPath = Environment.CurrentDirectory;
+            string relPath = absPath + "/data";
+            string[] nameFiles = Directory.GetFiles(relPath, "*.txt");
+
+            for (int i = 0; i < nameFiles.Length; i++)
+            {
+                string[] fileNames = File.ReadAllLines(nameFiles[i]);
+                for (int x = 0; x < fileNames.Length; x++)
+                {
+                    
+                    names.Add(fileNames[x]);
+                }
+            }
+
+        }
+        private void sortAlph()
+        {
+            Alphnames.Sort();
+            for (int i = 0; i < Alphnames.Count; i++)
+            {
+                rvrsAlphNames.Add(Alphnames[i]);
+                lstAlph.Items.Add(Alphnames[i]);
+            }
+        }
+        private void sortRvrsAlph()
+        {
+            rvrsAlphNames.Reverse();
+            for (int i = 0; i < rvrsAlphNames.Count; i++)
+            {
+                lstRvrsAlph.Items.Add(rvrsAlphNames[i]);
+
+            }
+        }
+        private void sortLastName()
+        {
+            for (int i = 0; i < names.Count; i++)
+            {
+                string name = names[i];
+                string[] lastName = name.Split(' ');
+                lastNames.Add(lastName[1]);
+
+                lstLastName.Items.Add(lastNames[i]);
+            }
+        }
+  
     }
 }
